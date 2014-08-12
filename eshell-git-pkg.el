10fc4c2428dff4bfdb48304e1a7bae73900e5e50
Koichi Osanai <osanai3@gmail.com>
2014-08-12 23:54:15 +0900

    add pack script

---
+++ eshell-git-pkg.el
@@ -0,0 +1,1 @@
+(define-package "eshell-git" "0.1" "git frontend for eshell")
---
+++ pack.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VERSION=0.1
+NAME=eshell-git
+DIR=$NAME-$VERSION
+TAR=$NAME-$VERSION.tar
+mkdir -p $DIR
+cp *.el README $DIR
+tar -c -f $TAR $DIR
+rm -rf $DIR
